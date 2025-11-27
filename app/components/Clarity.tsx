751c53e8d295404668f74be39774e53f180a8ed6
eduardo.rodriguez <eduardo.rodriguez@usgbc.org>
2025-11-26 20:34:31 -0500

    feat: exlclude localhost from clarity

--- app/components/Clarity.tsx
+++ app/components/Clarity.tsx
@@ -7,6 +7,8 @@ import { SettingsHandler } from "../handlers/settings/index.handler";
 
 export default function ClarityInitializer() {
   useEffect(() => {
+    if (window.location.hostname.includes("localhost")) return;
+
     console.info("Initializing Clarity Analytics");
     Clarity.init(SettingsHandler().clarityProjectId);
   }, []);
